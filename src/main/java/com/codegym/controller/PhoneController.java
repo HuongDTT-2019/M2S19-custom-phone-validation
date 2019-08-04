3d1a468e259e7a1d6ee5c83429f87cd8b1496ea5
Huong-DTT <thuhuong160994@gmail.com>
2019-08-04 14:37:00 +0700

    custom-phone-validation

---
+++ src/main/java/com/codegym/controller/PhoneController.java
@@ -0,0 +1,34 @@
+package com.codegym.controller;
+
+
+import com.codegym.model.PhoneNumber;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+
+@Controller
+public class PhoneController  {
+    @GetMapping("/")
+    public String showFrom(Model model){
+        model.addAttribute("phoneNumbers",new PhoneNumber());
+        return "index";
+    }
+    @PostMapping("/")
+    public String phoneValidation(@Valid @ModelAttribute("phoneNumbers") PhoneNumber phoneNumber, BindingResult bindingResult, Model model){
+        new PhoneNumber().validate(phoneNumber, bindingResult);
+        if (bindingResult.hasFieldErrors()){
+            return "index";
+        }
+        else {
+            model.addAttribute("phoneNumber",phoneNumber.getNumber());
+            return "result";
+        }
+    }
+}
